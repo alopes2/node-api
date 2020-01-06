ef32d43ec6b3ede50b39e6c7bfd20919d648e909
Andre Lopes <andrelo@dunnhumby.com>
2020-01-06 10:56:16 +0100

    Handle basic CRUD operations

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,54 +1,191 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
 
-exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1,
-        title: 'First post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
-        creator: {
-          name: 'Andre Lopes'
-        },
-        createdAt: new Date()
-      }
-    ]
-  });
+exports.getPosts = async (req, res, next) => {
+  const page = req.query.page || 1;
+  const pageSize = 2;
+  try {
+    const totalItems = await Post.find().countDocuments();
+
+    const posts = await Post.find()
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+
+    res.status(200).json({
+      message: 'Fetched posts successfully.',
+      posts: posts,
+      totalItems: totalItems
+    });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+
+    next(e);
+  }
 };
 
 exports.createPost = async (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(422).json({
-      message: 'Validation failed, entered data is incorrect.',
-      errors: errors.array()
-    });
-  }
-
-  const title = req.body.title;
-  const content = req.body.content;
-
-  const post = new Post({
-    title: title,
-    content: content,
-    imageUrl: 'images/duck',
-    creator: { name: 'Andre Lopes' }
-  });
-
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect.');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    if (!req.file) {
+      const error = new Error('No image provided.');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const title = req.body.title;
+    const content = req.body.content;
+    const imageUrl = req.file.path.replace('\\', '/');
+
+    const post = new Post({
+      title: title,
+      content: content,
+      imageUrl: imageUrl,
+      creator: { name: 'Andre Lopes' }
+    });
+
     var result = await post.save();
 
     console.log(result);
-  
+
     res.status(201).json({
       message: 'Post successfully created!',
       post: result
     });
-  } catch(e) {
-    console.log(e);
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+
+    next(e);
   }
+};
+
+exports.getPost = async (req, res, next) => {
+  const postId = req.params.postId;
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error = new Error('Post not found.');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ message: 'Post fetched.', post: post });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+
+    next(e);
+  }
+};
+
+exports.updatePost = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect.');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+
+    if (req.file) {
+      imageUrl = req.file.path.replace('\\', '/');
+    }
+
+    if (!imageUrl) {
+      const error = new Error('No file picked.');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error = new Error('Post not found.');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl);
+    }
+
+    post.title = title;
+    post.content = content;
+    post.imageUrl = imageUrl;
+
+    var result = await post.save();
+
+    console.log(result);
+
+    res.status(200).json({
+      message: 'Post successfully updated!',
+      post: result
+    });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+
+    next(e);
+  }
+};
+
+exports.deletePost = async (req, res, next) => {
+  const postId = req.params.postId;
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error = new Error('Post not found.');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    clearImage(post.imageUrl);
+
+    var result = await post.remove();
+
+    console.log(result);
+
+    res.status(200).json({
+      message: 'Post successfully deleted!',
+      post: result
+    });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+
+    next(e);
+  }
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+
+  fs.unlink(filePath, err => console.log(err));
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const { body } = require('express-validator');
+const updload = require('multer');
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
+
+const fileStorage = updload.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const filefilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
@@ -11,6 +33,7 @@ router.get('/posts', feedController.getPosts);
 // POST /feed/posts
 router.post(
   '/post',
+  updload({ storage: fileStorage, fileFilter: filefilter}).single('image'),
   [
     body('title', 'Title is required and have at least 5 chars.')
       .trim()
@@ -20,6 +43,30 @@ router.post(
       .isLength({ min: 5 })
   ],
   feedController.createPost
+);
+
+// GET /feed/posts/123
+router.get('/post/:postId', feedController.getPost);
+
+// PUT /feed/post/123
+router.put(
+  '/post/:postId',
+  updload({ storage: fileStorage, fileFilter: filefilter}).single('image'),
+  [
+    body('title', 'Title is required and have at least 5 chars.')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content', 'Content is required and have at least 5 chars.')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
+// DELETE /feed/post/123
+router.delete(
+  '/post/:postId',
+  feedController.deletePost
 );
 
 module.exports = router;
